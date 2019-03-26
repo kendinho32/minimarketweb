2c9562ee71d64c20fdf8ba93c661a068bc6066ac
knavarro <knavarro@everis.com>
2019-03-26 18:02:41 -0300

    modificaciones para el slider principal

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { SliderService } from '../services/slider.service';
+import { environment } from '../../environments/environment';
+import { Slider } from '../models/slider';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styles: []
+  styles: [],
+  providers: [SliderService]
 })
 export class SliderComponent implements OnInit {
 
-  constructor() { }
+  public url;
+  public response: any;
+  public sliders: Slider[];
+  public sliderFirst: Slider;
+
+  constructor(private sliderService: SliderService) {
+      this.url = environment.apiBase;
+  }
 
   ngOnInit() {
+      this.getAllSlider();
+  }
+
+  getAllSlider() {
+      this.sliderService.getSliders().subscribe(
+            response => {
+               this.response = response;
+               console.log(this.response);
+               if (this.response.success) {
+                this.sliders = this.response.data;
+                this.sliderFirst = this.sliders[0];
+                this.sliders.splice(0, 1);
+                console.log(this.sliders);
+               }
+            },
+            error => {
+                console.log(error);
+            }
+        );
   }
 
 }
