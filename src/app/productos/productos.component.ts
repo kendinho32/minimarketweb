6f36153087b64edcd1205b8e6e761fbbda590911
knavarro <knavarro@everis.com>
2019-04-09 12:21:52 -0400

    actualizacion

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductoService } from '../services/producto.service';
 import { environment } from '../../environments/environment';
 
+declare var $: any;
+
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
@@ -13,6 +15,7 @@ export class ProductosComponent implements OnInit {
   public url;
   public response;
   public productos;
+  public idCategoria: number;
 
   constructor(private productoService: ProductoService) {
     this.url = environment.apiBase;
@@ -20,6 +23,7 @@ export class ProductosComponent implements OnInit {
 
   ngOnInit() {
       this.getAllProductos();
+      $('html, body').animate({scrollTop: 0}, 'slow');
   }
 
   getAllProductos() {
@@ -34,6 +38,26 @@ export class ProductosComponent implements OnInit {
           console.log(error);
       }
     );
+  }
+
+  buscarProductosByCategoria(event) {
+    this.idCategoria = event.idCategoria;
+
+    if (this.idCategoria === 0) {
+        this.getAllProductos();
+    } else {
+        this.productoService.getProductsByCategoria(this.idCategoria).subscribe(
+            response => {
+                this.response = response;
+                if (this.response.success) {
+                    this.productos = this.response.data;
+                }
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
   }
 
 }
